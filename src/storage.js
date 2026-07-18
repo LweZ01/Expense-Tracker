751eb76a47abe8cfc535f4fa7ec766681deb1061
LweZ01 <144471422+LweZ01@users.noreply.github.com>
2026-07-18 03:42:50 -0400

    Final storage.js

--- src/storage.js
+++ src/storage.js
@@ -6,17 +6,40 @@ const DATA_FILE = join(import.meta.dirname, "..", "expenses.json");
 const readExpenses = async () => {
   try {
     const data = await fs.readFile(DATA_FILE, "utf-8");
+    if (data && data.length > 0) {
+      return JSON.parse(data);
+    } else {
+      return [];
+    }
   } catch (error) {
     if (error.code === "ENOENT") {
       return [];
     } else {
-      console.error("Error", error);
+      throw error;
     }
   }
+};
 
-  if (data && data.length > 0) {
-    return JSON.parse(data);
-  } else {
-    return [];
+const writeExpenses = async (expenses) => {
+  const writedata = JSON.stringify(expenses, null, 2);
+
+  try {
+    await fs.writeFile(DATA_FILE, writedata, "utf-8");
+  } catch (error) {
+    console.error("Error:", error);
   }
 };
+
+const getNextId = async (expenses) => {
+  try {
+    if (expenses.length === 0) {
+      return 1;
+    } else {
+      return Math.max(...expenses.map((e) => e.id)) + 1;
+    }
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
+
+export { readExpenses, writeExpenses, getNextId };
